336d7cda355a5df8b83c3fed515cd7fcd714293e
Ocean <mintteacupofficial@gmail.com>
2023-11-11 17:23:52 +0000

    changed text

--- rssparser.py
+++ rssparser.py
@@ -4,4 +4,4 @@ url = "https://nyaa.si/?page=rss"
 feed = feedparser.parse(url)
 
 for entry in feed.entries:
-    print(f"Entry Title: {entry.title} \n\nTorrent Link: {entry.link} \n\nPublished Date: {entry.published} \n\n----------------------------------\n\n")
+    print(f"Title: {entry.title} \n\nTorrent Link: {entry.link} \n\nPublished Date: {entry.published} \n\n----------------------------------\n\n")
