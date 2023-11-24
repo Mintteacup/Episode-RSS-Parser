7de17cb5a7cec829dc6bcf84f2ff58dd23692c73
Ocean <mintteacupofficial@gmail.com>
2023-11-24 17:11:43 +0000

    Improved code structure

--- rssparser.py
+++ rssparser.py
@@ -3,46 +3,50 @@ import csv
 import os
 
 #Deletes the csv file so no duplicates
-try:
-    os.remove("rss_data.csv")
-except OSError:
-    pass
-
-rss_feed_urls = [
-    "https://nyaa.si/?page=rss&u=Ember_Encodes",
-    "https://nyaa.si/?page=rss&u=subsplease"
-    ]
-
-quality = "1080p"
-title = ["Jujutsu Kaisen", "Sousou no Frieren", "Undead Unluck", "Dr. Stone", "Kusuriya no Hitorigoto"]
+def delete_csv_file(file_path):
+    try:
+        os.remove(file_path)
+    except OSError:
+        pass
 
 #Function to get feed data
-def fetch_rss_data(url):
-    global feed
+def fetch_rss_data(url, quality, title):
     feed = feedparser.parse(url)
     print(f"Feed Title: {feed.feed.title}")
+
     #Loops through every entry then prints them
     for entry in feed.entries:
-        if quality in entry.title:
-            if any(match in entry.title for match in title):
-                print(f"Title: {entry.title} \n\nTorrent Link: {entry.link} \n\nEntry Link: {entry.guid} \n\nPublished Date: {entry.published} \n\n----------------------------------\n\n")
-        else:
-            pass
+        if quality in entry.title and any(match in entry.title for match in title):
+            print(f"Title: {entry.title} \n\nTorrent Link: {entry.link} \n\nEntry Link: {entry.guid} \n\nPublished Date: {entry.published} \n\n----------------------------------\n\n")
 
-def to_csv():
+    return feed.entries
+
+def write_to_csv(entries, file_path, quality, title):
     #Prepares the CSV file
-    with open("rss_data.csv", mode="a", newline='', encoding='utf-8') as csv_file:
+    with open(file_path, mode="a", newline='', encoding='utf-8') as csv_file:
         fieldnames = ["Title", "Torrent Link", "Entry Link", "Published", "Summary"]
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        #writer.writeheader()
-        #Iterates through entries and write to the CSV file
-        for entry in feed.entries:
-            if quality in entry.title:
-                if any(match in entry.title for match in title):
-                    writer.writerow({"Title": entry.title, "Torrent Link": entry.link, "Entry Link": entry.guid, "Published": entry.published, "Summary": entry.summary})
-            else:
-                pass
 
-for url in rss_feed_urls:
-    fetch_rss_data(url)
-    to_csv()
+        #Iterates through entries and write to the CSV file
+        for entry in entries:
+            if quality in entry.title and any(match in entry.title for match in title):
+                writer.writerow({"Title": entry.title, "Torrent Link": entry.link, "Entry Link": entry.guid, "Published": entry.published, "Summary": entry.summary})
+
+def main():
+    rss_feed_urls = [
+        "https://nyaa.si/?page=rss&u=Ember_Encodes",
+        "https://nyaa.si/?page=rss&u=subsplease"
+    ]
+
+    quality = "1080p"
+    title = ["Jujutsu Kaisen", "Sousou no Frieren", "Undead Unluck", "Dr. Stone", "Kusuriya no Hitorigoto"]
+    
+    csv_file_path = "rss_data.csv"
+    
+    delete_csv_file(csv_file_path)
+
+    for url in rss_feed_urls:
+        entries = fetch_rss_data(url, quality, title)
+        write_to_csv(entries, csv_file_path, quality, title)
+
+main()
